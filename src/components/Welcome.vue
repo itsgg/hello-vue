af4bf0f07bf08e0413b31fe4d64cf03a08007488
Ganesh Gunasegaran <me@itsgg.com>
2021-12-03 18:08:33 +0530

    Initial commit

---
+++ src/components/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello {{ name }}!</h1>
+    <p>You are {{ age }} years old!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Ganesh Gunasegaran',
+      age: 30,
+    };
+  },
+};
+</script>
